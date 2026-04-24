92b4e2fd3a2ae4a7f2e5442aaf8b39e64a1402d1
kjrmyjn <kojiromyojin@gmail.com>
2026-04-24 20:35:51 +0900

    Remove GA and Meta Pixel tracking scripts
    Removed Google Analytics and Meta Pixel tracking scripts from layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,9 +27,7 @@ const notoSansJP = Noto_Sans_JP({
   adjustFontFallback: true,
 })
 
-const GA_ID = 'G-B7M920RCGR'
-const META_PIXEL_ID = '1658477098524563'
-const GTM_ID = 'GTM-PCBG8HPX'
+const GTM_ID = 'GTM-PW2KDRFF'
 const SITE_URL = 'https://sekaistay.com'
 
 export const metadata: Metadata = {
@@ -168,12 +166,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
         </Script>
         {/* End Google Tag Manager */}
-
-        {/* Meta Pixel — Facebook / Instagram 広告計測 */}
-        <Script id="meta-pixel-init" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','${META_PIXEL_ID}');fbq('track','PageView');`}
-        </Script>
-        {/* End Meta Pixel */}
       </head>
       <body>
         {/* GTM noscript: JavaScript無効環境でのフォールバック */}
@@ -183,16 +175,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* Meta Pixel noscript: JavaScript無効環境でのフォールバック */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-            alt=""
           />
         </noscript>
         <Suspense fallback={null}>
